0da4188005b26a79d533f9ed5dea651977a49942
agnieszka <agnieszka.gfkm@o2.pl>
2020-02-26 14:20:07 +0100

    [create] project skeleton

---
+++ src/main/java/com/agacorporation/springbootimage_uplouder/SpringbootImageUplouderApplication.java
@@ -0,0 +1,13 @@
+package com.agacorporation.springbootimage_uplouder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootImageUplouderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootImageUplouderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agacorporation/springbootimage_uplouder/SpringbootImageUplouderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agacorporation.springbootimage_uplouder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootImageUplouderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
