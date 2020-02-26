7f102a29e526f5ef8dba8f8e62b025c5f2901871
agnieszka <agnieszka.gfkm@o2.pl>
2020-02-26 16:04:18 +0100

    [change] config

--- src/main/java/com/agacorporation/springbootimage_uplouder/WebSecurityConfig.java
+++ src/main/java/com/agacorporation/springbootimage_uplouder/WebSecurityConfig.java
@@ -1,9 +1,37 @@
 package com.agacorporation.springbootimage_uplouder;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(new User("test", passwordEncoder().encode("test"), Collections.singleton(new SimpleGrantedAuthority("user"))));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests()
+               .antMatchers("/test1").authenticated()
+               .and()
+               .formLogin().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+return new BCryptPasswordEncoder();
+    }
 }
