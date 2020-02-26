adf4ab8948c190a67e5b8ff020c8b59e1e0a7631
agnieszka <agnieszka.gfkm@o2.pl>
2020-02-26 16:37:13 +0100

    [change] loadUserByUsername method to return appUserRepo.findByUsername

--- src/main/java/com/agacorporation/springbootimage_uplouder/UserDetailsServiceImpl.java
+++ src/main/java/com/agacorporation/springbootimage_uplouder/UserDetailsServiceImpl.java
@@ -1,13 +1,20 @@
 package com.agacorporation.springbootimage_uplouder;
 
+import com.agacorporation.springbootimage_uplouder.repo.AppUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private AppUserRepo appUserRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return appUserRepo.findByUsername(username);
     }
 }
