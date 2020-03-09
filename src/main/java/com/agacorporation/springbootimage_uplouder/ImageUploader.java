1ebc1ae2d006831ba37b9a53f3a8dc7e30fc8a08
agnieszka <agnieszka.gfkm@o2.pl>
2020-03-09 09:32:17 +0100

    [add] ImageUploader class

--- src/main/java/com/agacorporation/springbootimage_uplouder/ImageUploader.java
+++ src/main/java/com/agacorporation/springbootimage_uplouder/ImageUploader.java
@@ -17,17 +17,20 @@ public class ImageUploader {
                 "api_key", "174487827877899",
                 "api_secret", "uP5O2sWhCsW7eMvSeA9xIYe2Vyk"));
     }
-    public static void main(String[] args) throws IOException {
 
-        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dcchvnsqf",
-                "api_key", "174487827877899",
-                "api_secret", "uP5O2sWhCsW7eMvSeA9xIYe2Vyk"));
+    public String uploadFile(String path){
+        File file = new File(path);
+        Map uploadResult=null;
+        try {
+            uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        } catch (IOException e) {
+            //todo
 
-        File file = new File("C:\\stream\\img.jpg");
-        Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        }
+        return "";
 
     }
+
 
 
 }
